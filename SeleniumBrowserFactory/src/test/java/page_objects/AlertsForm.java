c37e7952651334f4a6f78bf2d6f8ff5e31441c95
Lisa Phan <liza.phan.truong@gmail.com>
2023-04-26 16:42:39 +0300

    Handle alert with confirmation

--- SeleniumBrowserFactory/src/test/java/page_objects/AlertsForm.java
+++ SeleniumBrowserFactory/src/test/java/page_objects/AlertsForm.java
@@ -1,10 +1,13 @@
 package page_objects;
 
 import elements.Button;
+import elements.Label;
 import org.openqa.selenium.By;
 
 public class AlertsForm extends BasePage {
     private final Button alert = new Button(By.xpath("//button[contains(@id,'alert')]"), "Alert");
+    private final Button confirmAlert = new Button(By.xpath("//button[contains(@id,'confirm')]"), "Confirm alert");
+    private final Label confirmResult = new Label(By.xpath("//span[contains(@id,'confirmResult')]"), "Confirm result");
 
     public AlertsForm() {
         super(new Button(By.xpath("//button[contains(@id,'alert')]"), "Alert"), "Alert Form");
@@ -12,5 +15,13 @@ public class AlertsForm extends BasePage {
 
     public void clickOnAlertButton() {
         alert.click();
+    }
+
+    public void clickOnConfirmAlertButton() {
+        confirmAlert.click();
+    }
+
+    public String getConfirmResultText() {
+        return confirmResult.getText();
     }
 }
--- SeleniumBrowserFactory/src/test/java/test_cases/TestCase1.java
+++ SeleniumBrowserFactory/src/test/java/test_cases/TestCase1.java
@@ -19,10 +19,11 @@ public class TestCase1 extends BaseTest {
         MenuPage menuPage = new MenuPage();
         menuPage.openForm(MenuPage.Form.ALERTS);
         AlertsForm alertsForm = new AlertsForm();
-        Assert.assertTrue(alertsForm.isPageOpen(), "Alert form dod not open");
+        Assert.assertTrue(alertsForm.isPageOpen(), "Alert form did not open");
 
         System.out.println("STEP 3 : Open alert");
         alertsForm.clickOnAlertButton();
+        Assert.assertTrue(browser.isAlertPresent(), "Alert did not open");
         String expectedAlertMessage = testData.get("alert-msg").toString();
         String actualAlertMessage = browser.getAlertText();
         Assert.assertEquals(actualAlertMessage, expectedAlertMessage, "Unexpected alert's message");
@@ -30,5 +31,19 @@ public class TestCase1 extends BaseTest {
         System.out.println("STEP 4 : Accept alert");
         browser.acceptAlert();
         Assert.assertFalse(browser.isAlertPresent(), "Alert did not close");
+
+        System.out.println("STEP 5. Open confirm alert");
+        alertsForm.clickOnConfirmAlertButton();
+        Assert.assertTrue(browser.isAlertPresent(), "Alert did not open");
+        String actualConfirmAlertMessage = browser.getAlertText();
+        String expectedConfirmAlertMessage = testData.get("confirm-alert-msg").toString();
+        Assert.assertEquals(actualConfirmAlertMessage, expectedConfirmAlertMessage, "Unexpected alert message");
+
+        System.out.println("STEP 6 : Accept confirm alert");
+        browser.acceptAlert();
+        Assert.assertFalse(browser.isAlertPresent(), "Alert did not close");
+        String actualConfirmResultMessage = alertsForm.getConfirmResultText();
+        String expectedConfirmResultMessage = testData.get("accept-confirm-alert-msg").toString();
+        Assert.assertEquals(actualConfirmResultMessage, expectedConfirmResultMessage, "Confirm alert result does not match");
     }
 }
