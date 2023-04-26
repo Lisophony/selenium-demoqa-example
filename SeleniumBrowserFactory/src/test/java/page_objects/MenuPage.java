b2e1682f43d04d475820a96c7f9987d87c234bbf
Lisa Phan <liza.phan.truong@gmail.com>
2023-04-26 15:13:10 +0300

    Add new page object - MenuPage. Used to switch form mostly

---
+++ SeleniumBrowserFactory/src/test/java/page_objects/MenuPage.java
@@ -0,0 +1,30 @@
+package page_objects;
+
+import elements.Button;
+import elements.Label;
+import org.openqa.selenium.By;
+
+public class MenuPage extends BasePage {
+    private final String formsLoc = "//div[contains(@class,'accordion')]//span[contains(@class,'text') and contains(text(),'%s')]";
+    public enum Form {
+        ALERTS("Alerts");
+
+        final String formName;
+        Form(String formName) {
+            this.formName = formName;
+        }
+
+        public String getFormName() {
+            return formName;
+        }
+    }
+
+    public MenuPage() {
+        super(new Label(By.xpath("//div[contains(@class,'accordion')]"), "Accordion"), "Menu Page");
+    }
+
+    public void openForm(Form formType) {
+        By formLocator = By.xpath(String.format(formsLoc, formType.getFormName()));
+        new Button(formLocator, formType.formName).click();
+    }
+}
