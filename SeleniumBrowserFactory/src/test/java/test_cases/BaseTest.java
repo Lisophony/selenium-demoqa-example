de0eed8b10ce49ae6e3a59c9f1ddb11c485858ba
Lisa Phan <liza.phan.truong@gmail.com>
2023-04-26 16:12:46 +0300

    Add test data source

--- SeleniumBrowserFactory/src/test/java/test_cases/BaseTest.java
+++ SeleniumBrowserFactory/src/test/java/test_cases/BaseTest.java
@@ -2,11 +2,14 @@ package test_cases;
 
 import browser.Browser;
 import browser.BrowserManager;
+import org.json.simple.JSONObject;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import utils.JsonUtil;
 
 public abstract class BaseTest {
     protected Browser browser;
+    JSONObject testData = JsonUtil.getJson("src/test/resources/test.json");
 
     @BeforeMethod
     public void setup() {
