3600cdd31d3d4605ee463253204071ea7925d194
Lisa Phan <liza.phan.truong@gmail.com>
2023-04-27 09:24:41 +0300

    Implement method find()

--- SeleniumBrowserFactory/src/main/java/elements/BaseElement.java
+++ SeleniumBrowserFactory/src/main/java/elements/BaseElement.java
@@ -24,7 +24,7 @@ public class BaseElement {
     }
 
     protected WebElement find() {
-        return null;
+        return BrowserManager.getBrowser().getDriver().findElement(locator);
     }
 
     public boolean exists() {
