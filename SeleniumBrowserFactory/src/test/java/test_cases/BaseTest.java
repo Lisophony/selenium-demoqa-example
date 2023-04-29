921a5c48613ba489c52ab4a89e09e6b5afbf1028
Lisa Phan <liza.phan.truong@gmail.com>
2023-04-29 16:03:36 +0300

    Change annotations

--- SeleniumBrowserFactory/src/test/java/test_cases/BaseTest.java
+++ SeleniumBrowserFactory/src/test/java/test_cases/BaseTest.java
@@ -3,20 +3,20 @@ package test_cases;
 import browser.Browser;
 import browser.BrowserManager;
 import org.json.simple.JSONObject;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import utils.JsonUtil;
 
 public abstract class BaseTest {
     protected Browser browser;
     JSONObject testData = JsonUtil.getJson("src/test/resources/test.json");
 
-    @BeforeMethod
+    @BeforeClass
     public void setup() {
         browser = BrowserManager.getBrowser();
     }
 
-    @AfterMethod
+    @AfterClass
     public void tearDown() {
         browser.close();
     }
